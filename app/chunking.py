97c7f2853e83dc1c941a31b6c66eb8ac475ee957
Rudraksh <rudrakshwarang75@gmail.com>
2025-11-14 02:57:08 +0530

    Add chunking logic

---
+++ app/chunking.py
@@ -0,0 +1,22 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def get_text_splitter():
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 500,
+        chunk_overlap =100,
+        length_function = len,
+        sperator = ["\n\n", "\n", " ", ""] 
+    )
+    return splitter
+
+def chunk_text(text : str):
+    splitter = get_text_splitter()
+    chunks =splitter.split_text(text)
+
+    structured_chunks=[]
+    for i, chunk in enumerate(chunks):
+        structured_chunks.append({
+            "chunk_id": i,
+            "text": chunk
+        })
+    return structured_chunks
