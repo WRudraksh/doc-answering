b7f63a3715f1b3944c99d4e71d6e145897e46749
Rudraksh <rudrakshwarang75@gmail.com>
2025-11-15 02:40:21 +0530

    Added Vectordb

---
+++ app/vectordb.py
@@ -0,0 +1,25 @@
+import faiss
+import numpy as np
+import pickle
+from typing import List, Dict
+
+class FaissStore:
+    def __init__(self, dim:int):
+        self.dim = dim
+        self.index = faiss.IndexFlatL2(dim)
+        self.metadatas: List[Dict] = []
+
+    def save(self, path_prefix: str):
+        faiss.write_index(self.index, f"{path_prefix}_index.index")
+        with open(f"{path_prefix}_meta.pkl", "wb") as f:
+            pickle.dump(self.metadatas, f)
+
+
+    @classmethod
+    def load(cls, path_prefix: str, dim: int):
+        inst = cls(dim)
+        inst.index = faiss.read_index(f"{path_prefix}_index.index")
+        with open(f"{path_prefix}_meta.pkl", "rb") as f:
+            inst.metadatas = pickle.load(f)
+        return inst
+    
