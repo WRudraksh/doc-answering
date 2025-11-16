f3e9f8a0f53dbe503798bacc36cd61487f79b2ec
Rudraksh <rudrakshwarang75@gmail.com>
2025-11-16 18:28:05 +0530

    updated llm

--- app/llm_client.py
+++ app/llm_client.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
+from app.config import OPENAI_API_KEY
 
-def get_llm_client(api_key: str):
+def get_llm_client():
+    api_key = OPENAI_API_KEY
     return OpenAI(api_key=api_key)
 
 def ask_llm(client, prompt: str, model: str="gpt-5.1"):
